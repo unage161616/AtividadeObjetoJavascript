7dc32ef9e30771c7bf932b06dff8143f112d72a1
unage161616 <64287663+unage161616@users.noreply.github.com>
2020-05-28 22:12:36 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,46 @@
+let receita = {
+ id:'2',
+ titulo = omelete,
+ porcao = 1,
+ ingredientes = ["ovo" , "queijo" , "frangodesfiado"]
+
+
+};
+console.log(receita.ingredientes[0]);
+console.log(receita.ingredientes[1]);
+console.log(receita.ingredientes[2]);
+
+
+var livro =  [{
+  titulo: 'harry potter',
+  author: 'desconhecido',
+  lido : true
+
+},
+{
+  titulo: 'crepusculo',
+  author: 'desconhecido',
+  lido : false
+
+}
+
+];
+
+for (i=o ; i<livro.length ; i++) {
+  console.log(livro[i])
+};
+
+ if (livro.lido = true) {
+   console.log ("o livro foi lido")
+ }
+
+else {
+
+  console.log ("livro ainda não foi lido")
+};
+
+
+
+
+
+

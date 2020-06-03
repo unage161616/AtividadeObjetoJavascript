d17c0bdd246220fc06be5947fc14242c9ff0ba57
unage161616 <64287663+unage161616@users.noreply.github.com>
2020-06-03 16:01:57 -0300

    Add files via upload

---
+++ AtividadeObjetos/js.js
@@ -0,0 +1,76 @@
+/*let receita = {
+ id:'2',
+ titulo : 'omelete',
+ porcao : 1,
+ ingredientes : ["ovo" , "queijo" , "frangodesfiado"]
+
+
+};
+console.log(receita.ingredientes[0]);
+console.log(receita.ingredientes[1]);
+console.log(receita.ingredientes[2]);
+
+
+var livro =  [{
+  id: 1,
+  titulo: 'harry potter',
+  author: 'desconhecido',
+  lido : true
+
+},
+{
+  id:2,
+  titulo: 'crepusculo',
+  author: 'desconhecido',
+  lido : false
+
+}
+
+];
+
+for ( i=0; i<livros.length; i++) {
+  console.log("livro" + livro[i]);
+};
+
+
+
+ if (livro.lido = true) {
+   console.log ("o livro foi lido")
+ }
+
+else {
+
+  console.log ("livro ainda não foi lido")
+};
+
+*/
+
+
+
+fetch ('https://reqres.in/api/users',{
+  method: 'POST',
+  body:{
+     name: 'user 1'
+
+  }
+
+
+}).then (res => {
+
+  return res.json ();
+})
+
+.then (data=>console.log(data))
+.catch (error=>console.log("error"))
+
+
+ 
+fetch ('https://dog.ceo/api/breeds/image/random')
+  .then(function (response){
+    return response.json (); 
+  })
+    .then (function (data){
+        console.log(data);
+    })
+
+  
